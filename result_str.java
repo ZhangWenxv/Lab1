632cd87ac96517a38d0723628bb0417bdc29c84f
Zhang Wenxu <2631453288@qq.com>
2016-11-21 23:43:01 +0800

    use for white changes

--- result_str.java
+++ result_str.java
@@ -1,4 +1,4 @@
-package lab1;
+package test6;
 
 import static org.junit.Assert.*;
 
@@ -6,22 +6,14 @@ import org.junit.Test;
 
 public class result_str {
 
-	@Test
-	public void testSol_ins2() {
-		String result=test6.sol_ins2("x*2*y*z*3*x+x*2","x=0 y=2");
-		assertEquals("0+0",result);
-	}
 
 	@Test
-	public void testSim_add() {
-		String result=test6.sim_add("22*x*z*y","2*x*y*z");
-		assertEquals("24*x*z*y",result);
+	public void testSol_ins2_part() {
+		String result=test6.sol_ins2_part("x*2*y*z*3*x","x=0 y=2");
+		assertEquals("0",result);
 	}
 
-	@Test
-	public void testSimp() {
-		String result=test6.simp("2*x*y+2*y*x+0+0");
-		assertEquals("4*x*y",result);
+
 	}
 
 }
